e8184e2ed470b55d021d856b6fa0cad2c60045b4
BlessyNamadi <63627802+BlessyNamadi@users.noreply.github.com>
2020-06-26 14:54:53 +0530

    Add files via upload

---
+++ leap.py
@@ -0,0 +1,12 @@
+n=int(input())
+if(n%4==0):
+    if(n%100==0):
+        if(n%400==0):
+            print("leap year")
+        else:
+            print("Not a leap year")
+    else:
+        print("leap year")
+else:
+    print("Not a leap year")
+        
